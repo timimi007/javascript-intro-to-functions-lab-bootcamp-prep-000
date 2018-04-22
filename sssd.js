0e2e8d69cd132e4fede1dfb544f2d35cb70b98b7
Oday <oday.altimimi@gmail.com>
2018-04-22 17:40:15 +0000

    automatically backed up by learn

---
+++ sssd.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+
+$('nobr:contains("MW Link Drop Site")').closest('tr').hide();
+$('nobr:contains("Estimated Material attached (Fiber Planning)")').closest('tr').hide();
+$('nobr:contains("Fiber Planning Comment")').closest('tr').hide();
+$('nobr:contains("Implmentation By (IP Planning)")').closest('tr').hide();
+
+});
+
+
+
+
